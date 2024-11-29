7265ffc7e42132d794ae979d7ead6d511c8dbde4
aryabhupathi <108666879+aryabhupathi@users.noreply.github.com>
2024-11-29 20:40:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const authRoutes = require("./Routes/AuthRoutes");
+// const busRoutes = require("./Routes/BusRoutes");
+// const trainRoutes = require("./Routes/TrainRoutes");
+// const flightRoutes = require("./Routes/FlightRoutes");
+// dotenv.config();
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log(err));
+// app.use('/', (req, res) => {
+//   res.send('Welcome to the homepage!');
+// });
+// app.use("/api/auth", authRoutes);
+// app.use("/api/bus", busRoutes);
+// app.use("/api/train", trainRoutes);
+// app.use("/api/flight", flightRoutes);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -17,12 +47,12 @@ mongoose
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to the homepage!');
 });
-app.use("/api/auth", authRoutes);
-app.use("/api/bus", busRoutes);
-app.use("/api/train", trainRoutes);
-app.use("/api/flight", flightRoutes);
+app.get("/api/auth", authRoutes);
+app.get("/api/bus", busRoutes);
+app.get("/api/train", trainRoutes);
+app.get("/api/flight", flightRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
